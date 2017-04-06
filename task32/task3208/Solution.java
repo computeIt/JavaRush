4ac6397a01cea61f4bd5c6a6a20c52d404c640a1
computeIt <pushok@mail.ru>
2017-04-06 13:53:29 +0300

    Update Solution.java
    #RMI2-#Remote-#Stub-#LocateRegistry

--- task32/task3208/Solution.java
+++ task32/task3208/Solution.java
@@ -1,4 +1,4 @@
-package com.javarush.task.task32.task3208;
+package com.javarush.task.task32.task3208;  
 import java.rmi.AlreadyBoundException;
 import java.rmi.NotBoundException;
 import java.rmi.Remote;
