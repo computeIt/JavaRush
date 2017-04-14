03542b3cad411e2c973a40b3fe7ea8530be0a37e
computeIt <pushok@mail.ru>
2017-04-14 14:10:34 +0300

    Update Solution.java

--- task33/task3304/Solution.java
+++ task33/task3304/Solution.java
@@ -38,17 +38,29 @@ public class Solution {
         StringWriter writer = new StringWriter();
         om.writeValue(writer, one);
 
+        /*тут важно понимать, что, например, если мы записывали в строку объект класса First следующего вида
+        First first = new First(100, "any string as a name")
+        то строка json внутри writer сейчас выглядит следующим образом
+        {"className":"first","i":100,"name":"any string as a name"}
+        мы просто меняем first на second, работая с json строкой, как с обычной, и запихиваем ее в StringReader,
+        откуда впоследствии вытаскиваем вместо json-строки - объект другого класса(т.к. java его считала другим классом в результате наших
+        манипуляций с объектом, пока тот был json строкой*/
         String oneClassName = one.getClass().getSimpleName().toLowerCase();
         String resClassName = resultClassObject.getSimpleName().toLowerCase();
         String jsonString = writer.toString().replaceFirst(oneClassName, resClassName);
+        /*после этой строки кода наша json строка стала выглядеть так
+        {"className":"second","i":100,"name":"any string as a name"}
+        мы подменили имя класса и в 57строке указываем, какой класс нам нужен в результате а именно - resultClassObject
+        данный класс совпадает с тем, который идет в самой строке, поэтому после десериализации мы получим объект класса Second*/
 
         StringReader sReader = new StringReader(jsonString);
         Object result = om.readValue(sReader, resultClassObject);
         return result;
     }
 
-    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME,  property="className")
-    @JsonSubTypes(@JsonSubTypes.Type(value=First.class,  name="first"))
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME,  property="className")/*тут мы задаем имя, которое будет фигурировать 
+    перед именем класса в json строке после сериализации - см строку 44*/
+    @JsonSubTypes(@JsonSubTypes.Type(value=First.class,  name="first"))/*это - изменение видимого имени класса*/
     public static class First {
         public int i;
         public String name;
