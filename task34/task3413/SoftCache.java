12af82000290848fd828d0266bed80ed50628047
computeIt <pushok@mail.ru>
2017-04-21 19:16:06 +0300

    Create SoftCache.java

---
+++ task34/task3413/SoftCache.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task34.task3413;
+
+import java.lang.ref.SoftReference;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SoftCache {
+    private Map<Long, SoftReference<AnyObject>> cacheMap = new ConcurrentHashMap<>();
+
+    public AnyObject get(Long key) {
+        if(!cacheMap.containsKey(key))
+            return null;
+        SoftReference<AnyObject> softReference = cacheMap.get(key);
+        return softReference.get();
+    }
+
+    public AnyObject put(Long key, AnyObject value) {
+        SoftReference<AnyObject> softReference = cacheMap.put(key, new SoftReference<>(value));
+
+        if(softReference == null)
+            return null;
+        else {
+            AnyObject aObj = softReference.get();
+            softReference.clear();
+            return aObj;
+        }
+    }
+
+    public AnyObject remove(Long key) {
+        if(!cacheMap.containsKey(key))
+            return null;
+        SoftReference<AnyObject> softReference = cacheMap.remove(key);
+        AnyObject aObj = softReference.get();
+        softReference.clear();
+        return aObj;
+    }
+}
