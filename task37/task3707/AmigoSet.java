05747b072ce0041d71122127e63b74c57f7b7265
computeIt <pushok@mail.ru>
2017-04-26 16:19:14 +0300

    Update AmigoSet.java

--- task37/task3707/AmigoSet.java
+++ task37/task3707/AmigoSet.java
@@ -3,25 +3,6 @@ package com.javarush.task.task37.task3707;
 import java.io.*;
 import java.util.*;
 
-/*Твое собственное множество AmigoSet реализует интерфейс Serializable. Однако, не сериализуется правильно.
-1. Реализуй свою логику сериализации и десериализации.
-Вспоминай, какие именно приватные методы нужно добавить, чтоб сериализация пошла по твоему сценарию.
-Для сериализации:
-* сериализуй сет
-* сериализуй capacity и loadFactor у объекта map, они понадобятся для десериализации.
-Т.к. эти данные ограничены пакетом, то воспользуйся утилитным классом HashMapReflectionHelper, чтобы достать их.
-Для десериализации:
-* вычитай все данные
-* создай мапу используя конструктор с capacity и loadFactor
-2. Помнишь, что такое transient?
-
-Требования:
-1. В классе AmigoSet должен содержаться private метод writeObject с одним параметром типа ObjectOutputStream.
-2. В классе AmigoSet должен содержаться private метод readObject с одним параметром типа ObjectInputStream.
-3. Объект сериализованный с помощью метода writeObject должен быть равен объекту десериализованному с помощью метода readObject.
-4. В методе writeObject должен быть вызван метод defaultWriteObject на объекте типа ObjectOutputStream полученном в качестве параметра.
-5. В методе readObject должен быть вызван метод defaultReadObject на объекте типа ObjectInputStream полученном в качестве параметра.*/
-
 public class AmigoSet<E> extends AbstractSet<E> implements Set<E>, Cloneable, Serializable{
     private static final Object PRESENT = new Object();
     private transient HashMap<E, Object> map;
@@ -43,7 +24,8 @@ public class AmigoSet<E> extends AbstractSet<E> implements Set<E>, Cloneable, Se
         oos.defaultWriteObject();
         oos.writeInt(HashMapReflectionHelper.<Integer>callHiddenMethod(map, "capacity"));
         oos.writeFloat(HashMapReflectionHelper.<Float>callHiddenMethod(map, "loadFactor"));
-        oos.writeObject(new HashSet<>((Collection<Integer>) map.keySet()));
+        for(E e : map.keySet())
+            oos.writeObject(e);
     }
 
     private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
@@ -51,7 +33,10 @@ public class AmigoSet<E> extends AbstractSet<E> implements Set<E>, Cloneable, Se
         int capacity = ois.readInt();
         float loadFactor = ois.readFloat();
         map = new HashMap<>(capacity, loadFactor);
-        addAll((Collection) ois.readObject());
+        for(int i=0; i<size(); i++){
+            E e = (E) ois.readObject();
+            map.put(e, PRESENT);
+        }
     }
 
     @Override
