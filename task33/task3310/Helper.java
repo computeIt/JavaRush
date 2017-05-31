e20de991dedd1c1a40e4e9d050736d2d0e885ed1
computeIt <pushok@mail.ru>
2017-05-31 18:21:13 +0300

    Create Helper.java

---
+++ task33/task3310/Helper.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task33.task3310;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+/**
+ * Created by Igor on 29.05.2017.
+ */
+public class Helper {
+
+    public static String generateRandomString(){
+        return new BigInteger(130, new SecureRandom()).toString(32);
+    }
+
+    public static void printMessage(String message){
+        System.out.println(message);
+    }
+}
