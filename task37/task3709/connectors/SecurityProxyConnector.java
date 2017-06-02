fdf7618925a399fb8971c9f72c273d51ef0fcbbd
computeIt <pushok@mail.ru>
2017-06-02 15:49:05 +0300

    Update SecurityProxyConnector.java

--- task37/task3709/connectors/SecurityProxyConnector.java
+++ task37/task3709/connectors/SecurityProxyConnector.java
@@ -21,3 +21,13 @@ public class SecurityProxyConnector implements Connector {
         }
     }
 }
+
+/*Требования:
+1. В классе SecurityProxyConnector должно быть создано поле типа SimpleConnector.
+2. В классе SecurityProxyConnector должно быть создано поле типа SecurityChecker.
+3. Конструктор класса SecurityProxyConnector должен принимать один параметр типа String и инициализировать поле типа SimpleConnector.
+4. Метод connect класса SecurityProxyConnector должен выполнять проверку безопасности с помощью вызова
+метода performSecurityCheck у объекта типа SecurityChecker.
+5. Если проверка безопасности была пройдена, должно быть выполнено подключение.
+6. Если проверка безопасности не была пройдена, подключение не должно быть выполнено.
+7. Класс SecurityProxyConnector должен поддерживать интерфейс Connector*/
