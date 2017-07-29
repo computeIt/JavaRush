260dc1ae50fdcd9d7f809cb908454fbe5e32fe18
computeIt <pushok@mail.ru>
2017-07-29 10:47:58 +0300

    Create Operation.java

---
+++ task26/task2613/Operation.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task26.task2613;
+
+/**
+ * Created by Андрей on 28.06.2017.
+ */
+public enum Operation {
+    INFO, DEPOSIT, WITHDRAW, EXIT;
+
+    public static Operation getAllowableOperationByOrdinal(Integer i){
+        switch (i) {
+            case 1: return Operation.INFO;
+            case 2: return Operation.DEPOSIT;
+            case 3: return Operation.WITHDRAW;
+            case 4: return Operation.EXIT;
+            default: throw new IllegalArgumentException();
+            }
+    }
+}
