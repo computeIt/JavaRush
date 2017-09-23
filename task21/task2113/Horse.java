a680d890689688d28f100adcf0807ac9f4513c7a
computeIt <pushok@mail.ru>
2017-09-23 19:12:08 +0300

    Create Horse.java

---
+++ task21/task2113/Horse.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task21.task2113;
+
+/**
+ * Created by Андрей on 23.09.2017.
+ */
+public class Horse {
+    private String name;
+    private double speed;
+    private double distance;
+
+    public Horse(String name, double speed, double distance) {
+        this.name = name;
+        this.speed = speed;
+        this.distance = distance;
+    }
+
+    public void move(){
+        distance += speed*Math.random();
+    }
+    public void print(){
+        int distance2 = (int)Math.floor(distance);
+        for(int i=0; i<distance2; i++){
+            System.out.print(".");
+        }
+        System.out.println(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+}
